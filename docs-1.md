cd35fcaebb140a48accff69696ea871d834ac518
super-rogatory <chukwudi10@gmail.com>
2021-08-18 21:09:03 -0700

    created background, notes in notebook

--- docs-1.md
+++ docs-1.md
@@ -1,1 +1,5 @@
 # Getting Started with Phaser 3
+## HTML 5 Game Engine Framework.
+
+
+## NOTE: This tutorial guide is dumping the global namespace of each js file on top of one another in index.html.
---
+++ src/game.js
@@ -0,0 +1,14 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: [ Example1 ]
+};
+
+let game = new Phaser.Game(config);
