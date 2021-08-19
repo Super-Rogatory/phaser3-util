a7f7d04e6be84ec30d150ad5412812f0ff68b3ec
super-rogatory <chukwudi10@gmail.com>
2021-08-18 23:59:16 -0700

    Added Animations

--- src/ForeGround.js
+++ src/ForeGround.js
@@ -1,32 +1,66 @@
 class ForeGround extends Phaser.Scene {
 	constructor() {
+		// [1]
 		super({ key: 'ForeGround', active: true });
 	}
 	createGround(x, y) {
+		// [2.2]
 		this.groundGroup.create(x, y, 'ground');
 	}
+	createAnimations() {
+		this.anims.create({
+			key: 'run',
+			frames: this.anims.generateFrameNumbers('mainCharacter', { start: 17, end: 20 }),
+			frameRate: 10,
+			repeat: -1,
+		});
+		this.anims.create({
+			key: 'jump',
+			frames: [{ key: 'mainCharacter', frame: 17 }],
+			frameRate: 20,
+		});
+		this.anims.create({
+			key: 'idleUnarmed',
+			frames: [{ key: 'mainCharacter', frame: 11 }],
+			frameRate: 10,
+		});
+		this.anims.create({
+			key: 'idleArmed',
+			frames: [{ key: 'mainCharacter', frame: 6 }],
+			frameRate: 10,
+		});
+	}
 	preload() {
+		// [1]
 		this.load.spritesheet('mainCharacter', 'assets/spriteSheets/josh.png', {
 			frameWidth: 340,
 			frameHeight: 460,
 		});
-        // still need to load the ground with a special sprite key
+		// [2.1.1]
+		// still need to load the ground with a special sprite key
 		this.load.image('ground', 'assets/sprites/ground.png');
-        // a group allows you to group together similar objects and control them as a single unit.
-        this.groundGroup = this.physics.add.staticGroup({ classType: Ground });
+		// [2.1.2]
+		// a group allows you to group together similar objects and control them as a single unit.
+		this.groundGroup = this.physics.add.staticGroup({ classType: Ground });
 	}
 	create() {
+		// [1]
 		this.player = new Player(this, 20, 400, 'mainCharacter').setScale(0.25);
+		// [2]
 		this.createGround(160, 540);
-        this.createGround(600, 540);
-        // collider() enables our player to physically collid with the ground, preventing him from falling through
-        this.physics.add.collider(this.player, this.groundGroup);
-        // allowing us to move the character body
-        this.cursors = this.input.keyboard.createCursorKeys();
+		this.createGround(600, 540);
+		// [2.2]
+		// collider() enables our player to physically collid with the ground, preventing him from falling through
+		this.physics.add.collider(this.player, this.groundGroup);
+		// [3.1]
+		// allowing us to move the character body
+		this.cursors = this.input.keyboard.createCursorKeys();
+		// create the animations
+		this.createAnimations();
 	}
-    update(time, delta) {
-        // the player object was created on line 19.
-        // the player class has the update method on it's prototype so we are able to utilize the update here.
-        this.player.update(this.cursors);
-    }
+	update(time, delta) {
+		// the player object was created on line 19.
+		// the player class has the update method on it's prototype so we are able to utilize the update here.
+		this.player.update(this.cursors);
+	}
 }
--- src/Player.js
+++ src/Player.js
@@ -1,34 +1,60 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y, spriteKey) {
-    super(scene, x, y, spriteKey);
-    this.scene = scene;
-    this.scene.add.existing(this);
-    // << INITIALIZE PLAYER ATTRIBUTES HERE >>
-    this.scene.physics.world.enable(this); // enables physics on this entity
-  }
-  updateMovement(cursors) {
-    // move left
-    if(cursors.left.isDown) {
-      this.setVelocityX(-360);
-    }
-    // move right
-    else if(cursors.right.isDown) {
-      this.setVelocityX(360);
-    }
-    else {
-      this.setVelocityX(0);
-    }
-  }
-  updateJump(cursors) {
-    // if the keyboard up key is being pressed AND the body of the player is touching the ground
-    if(cursors.up.isDown && this.body.touching.down) {
-      this.setVelocityY(-800);
-    }
-  }
-  // Check which controller button is being pushed and execute movement & animation
-  update(cursors) {
-    // RECALL: the update function will run on every frame. Note, the update function in the SCENE is what will be called on each frame.
-    this.updateMovement(cursors);
-    this.updateJump(cursors);
-  }
+	constructor(scene, x, y, spriteKey) {
+		super(scene, x, y, spriteKey);
+		this.scene = scene;
+		this.scene.add.existing(this);
+		// << INITIALIZE PLAYER ATTRIBUTES HERE >>
+		this.scene.physics.world.enable(this); // enables physics on this entity
+		this.facingLeft = false;
+	}
+	updateInAir() {
+		if (!this.body.touching.down) {
+			this.play('jump');
+		}
+	}
+	updateMovement(cursors) {
+		// Move left
+		if (cursors.left.isDown) {
+			if (!this.facingLeft) {
+				this.flipX = !this.flipX;
+				this.facingLeft = true;
+			}
+			this.setVelocityX(-360);
+			if (this.body.touching.down) {
+				this.play('run', true);
+			}
+		}
+		// Move right
+		else if (cursors.right.isDown) {
+			if (this.facingLeft) {
+				this.flipX = !this.flipX;
+				this.facingLeft = false;
+			}
+			this.setVelocityX(360);
+
+			if (this.body.touching.down) {
+				this.play('run', true);
+			}
+		}
+		// Neutral (no movement)
+		else {
+			this.setVelocityX(0);
+			// Whenever Josh is not moving, use the idleUnarmed animation
+			this.play('idleUnarmed');
+		}
+	}
+	updateJump(cursors) {
+		// if the keyboard up key is being pressed AND the body of the player is touching the ground
+		if (cursors.up.isDown && this.body.touching.down) {
+			this.setVelocityY(-800);
+		}
+	}
+	// Check which controller button is being pushed and execute movement & animation
+	update(cursors) {
+		// RECALL: the update function will run on every frame. Note, the update function in the SCENE is what will be called on each frame.
+		this.updateMovement(cursors);
+		this.updateJump(cursors);
+		// On update, check to see if Josh is in the air (see below)
+		this.updateInAir();
+	}
 }
